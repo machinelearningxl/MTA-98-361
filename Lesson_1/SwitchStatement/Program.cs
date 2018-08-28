d618cd2292380c7feff4bdb3be4e2b1abffb35fc
Antero Maripuu <antero.maripuu@gmail.com>
2018-08-28 23:17:28 +0100

    Commit

--- Lesson_1/SwitchStatement/Program.cs
+++ Lesson_1/SwitchStatement/Program.cs
@@ -10,9 +10,39 @@ namespace SwitchStatement
     {
         static void Main(string[] args)
         {
+            
+            TestSwith(3, 4, '+');
+            TestSwith(3, 4, '*');
+            TestSwith(3, 4, '5');
+            Console.ReadLine();
         }
 
+        public static void TestSwith(int op1, int op2, char opr)
+        {
+            int result=0;
+            switch (opr)
+            {
+                case '+': //if true
+                    result = op1 + op2;
+                    break;
+                case '-': // if true
+                    result = op1 - op2;
+                    break;
+                case '*':
+                    result = op1 * op2;
+                    break;
+                case '/':
+                    result = op1 / op2;
+                    break;
+                default:
+                    Console.WriteLine("Unknown operator");
+                    return;
+            }
 
+            Console.WriteLine("Result:{0}", result);
+            return;
+
+        }
 
     }
 }

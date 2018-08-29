6e7f5592b850628e6827ea0c64bd9e015857840c
Antero Maripuu <antero.maripuu@gmail.com>
2018-08-29 22:36:20 +0100

    Factorial

---
+++ Lesson_1/Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(4));
+            Console.ReadLine();
+        }
+    public static int Factorial (int n)
+        {
+            if (n == 0)
+            {
+                return 1; //base case
+
+            }
+            else
+            {
+
+                return n * Factorial(n - 1);
+            }
+
+        }
+    }
+}

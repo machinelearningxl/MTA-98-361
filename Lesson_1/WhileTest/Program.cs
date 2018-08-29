c96ae2720d1df2d8221b12ab42eebd6038068c9e
Antero Maripuu <antero.maripuu@gmail.com>
2018-08-29 21:32:29 +0100

    First

---
+++ Lesson_1/WhileTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WhileTest();
+            Console.ReadLine();
+        }
+     private static void WhileTest()
+        {
+            int i = 1;
+            while (i <= 5)
+            {
+
+                Console.WriteLine("The value of i = {0}", i);
+                i++;
+
+            }
+
+        }
+
+    }
+
+}

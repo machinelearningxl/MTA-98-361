059f6ef0187c03d18df7e878908e83d7ebd693cc
Antero Maripuu <antero.maripuu@gmail.com>
2018-08-29 22:22:07 +0100

    ForTest

---
+++ Lesson_1/ForTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTest
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            ForTest();
+            Console.ReadLine();
+        }
+
+    private static void ForTest()
+        {
+         for(int i=1; i<=5; i++)
+            {
+                Console.WriteLine("The value of i = {0}", i);
+            }
+
+
+        }
+    }
+}

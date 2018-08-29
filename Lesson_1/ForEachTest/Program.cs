338ec5cab4725e55c4da832a718de5010dc685cd
Antero Maripuu <antero.maripuu@gmail.com>
2018-08-29 22:29:16 +0100

    Forreach statement

---
+++ Lesson_1/ForEachTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program(); //Object created 
+            pr.ForEachTest();
+            Console.ReadLine();
+
+        }
+
+        void ForEachTest()
+        {
+            int[] numbers = {1,2,3,4,5};
+            foreach(int i in numbers)
+            {
+
+                Console.WriteLine("The value of i= {0}", i);
+            }
+
+        }
+    }
+}

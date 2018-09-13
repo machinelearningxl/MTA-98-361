9f37c634809b40911dee8b7e927ff05dd882533d
Antero Maripuu <antero.maripuu@gmail.com>
2018-09-13 17:01:11 +0100

    Handeling Exceptions

---
+++ Lesson_1/Competence_Assessment_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competence_Assessment_3
+{
+    class Program
+    {
+
+        private static double Divide(int x , int y)
+        {
+            double result;
+            result = x / y;
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

5603ffedcc5a99b578f0d07ec563975e303c0591
Antero Maripuu <antero.maripuu@gmail.com>
2018-09-17 22:27:57 +0100

    OOP first

--- Lesson_2/Rectangle/Program.cs
+++ Lesson_2/Rectangle/Program.cs
@@ -5,11 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Rectangle
-{
+{   
+
+    class Rectangle
+    {
+        private double lenght;
+        private double weight;
+
+        public Rectangle(double l, double w)
+        {
+
+            lenght = l;
+            weight = w;
+        }
+
+        public double getArea()
+        {
+            return lenght * weight;
+        }
+    }
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            Rectangle rect = new Rectangle(10, 10);
+            double area = rect.getArea();
+            Console.WriteLine("Area of the rectangle {0}", area);
+            Console.ReadLine();
+
         }
     }
 }
